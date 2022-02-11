21a492c47723070fe49fef75d3dd468667ba0b63
tYohannes <teklegy@gmail.com>
2022-02-11 15:14:26 +0300

    Create six component files

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+class TodoContainer extends Component {
+  render(){
+    return <div>
+      <h1>Hello from Create React App</h1>
+      <p>I'm in a React Component!</p>
+    </div>
+  }
+}
+
+export default TodoContainer;
