8dbf34d6ed437783aea51d35e6a9d918a954fa79
tYohannes <teklegy@gmail.com>
2022-02-11 15:18:19 +0300

    Use React.Fragment instead of div to wrap elemnts

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,10 +2,12 @@ import { Component } from 'react';
 
 class TodoContainer extends Component {
   render(){
-    return <div>
-      <h1>Hello from Create React App</h1>
-      <p>I'm in a React Component!</p>
-    </div>
+    return( 
+      <>
+        <h1>Hello from Create React App</h1>
+        <p>I'm in a React Component!</p>
+      </>
+    )
   }
 }
 
