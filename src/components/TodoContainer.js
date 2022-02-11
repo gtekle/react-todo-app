0a363ccbbf30b8b9f2e82d7d1b86e8ee22870e37
tYohannes <teklegy@gmail.com>
2022-02-11 16:16:01 +0300

    Add state to TodoContainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,32 @@
 import { Component } from 'react';
 
 class TodoContainer extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      },
+    ]
+  }
   render(){
     return( 
-      <>
-        <h1>Hello from Create React App</h1>
-        <p>I'm in a React Component!</p>
-      </>
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
     )
   }
 }
