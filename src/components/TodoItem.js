4920cda8dd252392a64a16a3065d3063e432afe1
tYohannes <teklegy@gmail.com>
2022-02-11 16:35:58 +0300

    Refactor TodoItem class component code to functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,5 @@
-import React from 'react';
-
-class TodoItem extends React.Component {
-  render(){
-    return <li>{this.props.todo.title}</li>
-  }
+function TodoItem(props) {
+  return <li>{props.todo.title}</li>
 }
 
 export default TodoItem;
