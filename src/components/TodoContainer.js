eb3d8dbe2500f5f9c9ee304223418d5e105de34f
tYohannes <teklegy@gmail.com>
2022-02-13 14:15:42 +0300

    Update todos list on componetDidMount lifecycle method from jsonplaceholder api

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -63,7 +63,7 @@ class TodoContainer extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ todos: data }));
   }
 
   render(){
