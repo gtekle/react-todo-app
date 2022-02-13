9022a334eacd640e906a7d6e7047e873bf03a7da
tYohannes <teklegy@gmail.com>
2022-02-13 13:44:27 +0300

    Add edit todo item functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,20 +10,32 @@ function TodoItem(props) {
   }
 
   const {completed, id, title } = props.todo;
-
+  
   return (
     <li className={styles.item}>
+      <div onDoubleClick={props.handleEditing} style={props.viewMode}>
+        <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)} />
+        <button onClick={() => props.deleteTodoProps(id)} >
+          Delete
+        </button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
+      </div>
       <input 
-      type="checkbox" 
-      className={styles.checkbox}
-      checked={completed} 
-      onChange={() => props.handleChangeProps(id)} />
-      <button onClick={() => props.deleteTodoProps(id)} >
-        Delete
-      </button>
-      <span style={completed ? completedStyle : null}>
-        {title}
-      </span>
+        type="text" 
+        style={props.editMode}
+        value={title}
+        className={styles.textInput} 
+        onChange={e => {
+          props.setUpdate(e.target.value, id)
+        }}
+        onKeyDown={props.handleUpdatedDone}
+      />
     </li>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,45 @@ import React from "react";
 import TodoItem from './TodoItem';
 
 class TodoList extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+  
+  handleUpdatedDone = (event) => {
+    if (event.key === "Enter") {
+    this.setState({ editing: false })
+  }
+  }
+
   render(){
+    let viewMode = {};
+    let editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return(
       <ul>
         {this.props.todos.map(todo => (
           <TodoItem 
-          key={todo.id} 
-          todo={todo} 
-          handleChangeProps={this.props.handleChangeProps}
-          deleteTodoProps={this.props.deleteTodoProps}
+            key={todo.id} 
+            todo={todo} 
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
+            handleEditing = {this.handleEditing}
+            viewMode = {viewMode}
+            editMode = {editMode}
+            editing = {this.state.editing}
           />
         ))}
       </ul>
