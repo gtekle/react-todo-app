c426ea22f347f3bdd8c94432b096e447029252d1
tYohannes <teklegy@gmail.com>
2022-02-14 22:34:06 +0300

    publish build

---
+++ build/precache-manifest.b0363c3d06e6713f97c591676f69abc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0545549f3bbcd1b83980b439da56447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b324e26c85d23b1afb",
+    "url": "/static/css/main.ac8b608d.chunk.css"
+  },
+  {
+    "revision": "426fd30239e4664c5f02",
+    "url": "/static/js/2.73ade813.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.73ade813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b324e26c85d23b1afb",
+    "url": "/static/js/main.d3679791.chunk.js"
+  },
+  {
+    "revision": "eb323c622e106faecb0e",
+    "url": "/static/js/runtime-main.341c860d.js"
+  }
+]);
