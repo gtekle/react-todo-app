d3ac16bc810c2872a3d3a7fdc1b725abcee57a70
tYohannes <teklegy@gmail.com>
2022-02-14 23:11:07 +0300

    Updates

---
+++ precache-manifest.d6d2b0585a0e990a8b22ef7ecfee06aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059e2616098fba6db89bd7185c9bda78",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a3b324e26c85d23b1afb",
+    "url": "/react-todo-app/static/css/main.ac8b608d.chunk.css"
+  },
+  {
+    "revision": "426fd30239e4664c5f02",
+    "url": "/react-todo-app/static/js/2.73ade813.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-todo-app/static/js/2.73ade813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b324e26c85d23b1afb",
+    "url": "/react-todo-app/static/js/main.d3679791.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
